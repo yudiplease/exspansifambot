27a87a833852910207fdc9a85bbde0d083bb077b
yudiplease <dregoinger@gmail.com>
2024-09-05 22:34:28 +0500

    add afk-command

--- src/main/java/dev/yudiplease/exspansi/bot/command/AfkCommand.java
+++ src/main/java/dev/yudiplease/exspansi/bot/command/AfkCommand.java
@@ -4,49 +4,53 @@ import dev.yudiplease.exspansi.bot.SendMessageToChannel;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
 import discord4j.core.object.command.ApplicationCommandInteractionOption;
 import discord4j.core.object.command.ApplicationCommandInteractionOptionValue;
-import discord4j.core.object.entity.Attachment;
 import discord4j.core.spec.EmbedCreateSpec;
 import discord4j.rest.util.Color;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Component
 @RequiredArgsConstructor
-public class PaymentCommand implements SlashCommand {
-
+public class AfkCommand implements SlashCommand {
     private final SendMessageToChannel sendMessageToChannel;
 
     @Override
     public String getName() {
-        return "payment";
+        return "afk";
     }
 
     @Override
     public Mono<Void> handle(ChatInputInteractionEvent event) {
-        String contract = event.getOption("contract")
+        ZoneId moscowZone = ZoneId.of("Europe/Moscow");
+        ZonedDateTime moscowTime = ZonedDateTime.now(moscowZone);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        String formattedTime = moscowTime.format(formatter);
+        String user = event.getInteraction().getUser().getId().asString();
+        String time = event.getOption("time")
                 .flatMap(ApplicationCommandInteractionOption::getValue)
                 .map(ApplicationCommandInteractionOptionValue::asString)
                 .get();
-        String statics = event.getOption("statics")
+        String reason = event.getOption("reason")
                 .flatMap(ApplicationCommandInteractionOption::getValue)
                 .map(ApplicationCommandInteractionOptionValue::asString)
-                .get();
-        Attachment proof = event.getOption("proof")
-                .flatMap(ApplicationCommandInteractionOption::getValue)
-                .map(ApplicationCommandInteractionOptionValue::asAttachment)
                 .get();
         EmbedCreateSpec spec = EmbedCreateSpec.builder()
-                .color(Color.BLUE)
-                .title("ЗАПРОС О ВЫПЛАТЕ ДЕНЕГ")
-                .addField("Запрос отправил: ", String.format("<@%s>", event.getInteraction().getUser().getId().asString()), true)
-                .addField("Название контракта", contract, false)
-                .addField("Статики игроков", statics, false)
-                .image(proof.getUrl())
+                .color(Color.RED)
+                .title("АФК отчёт")
+                .addField("Отчёт отправил: ", String.format("<@%s>", user), true)
+                .addField("Начало ухода: ", formattedTime, false)
+                .addField("Время ухода в АФК: ", time, false)
+                .addField("Причина: ", reason, false)
                 .build();
-        sendMessageToChannel.sendMessageToChannelAsEmbed(1221109587579637771l, spec);
+        sendMessageToChannel.sendMessageToChannelAsEmbed(1222217190871138334l, spec);
         return event.reply()
                 .withEphemeral(true)
-                .withContent("Запрос о получении выплаты отправлен.");
+                .withContent("АФК отчёт успешно отправлен!");
     }
 }
---
+++ src/main/java/dev/yudiplease/exspansi/bot/service/TaskService.java
@@ -0,0 +1,7 @@
+package dev.yudiplease.exspansi.bot.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+}
